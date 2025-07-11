32e83b1f2ebd7097e526c6e67fd0cf3949efe64a
Hasnain1408 <bsse1408@iit.du.ac.bd>
2025-07-11 17:22:01 +0600

    frontend modified code

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client';
 import SmartLibrarySystem from './smartLibrary';
 import './index.css';
 
+/* Tailwind CSS imports */
+// @tailwind base;
+// @tailwind components;
+// @tailwind utilities;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <SmartLibrarySystem />
   </React.StrictMode>
 );
+
+
+
